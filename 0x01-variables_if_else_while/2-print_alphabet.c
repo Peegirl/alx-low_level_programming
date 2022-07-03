f6362c99995ee1777e311cfb380c692de3d58866
Peegirl <timelessprecious2@gmail.com>
2022-07-03 13:33:09 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (success/correct)
+ * main -Entry point
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
 {
-	int first_nums;
-	int second_nums;
-	for (first_nums =0; first_nums < 100;
-			first_nums++)
-	{
-		for (second_nums=first_nums + 1;
-				second_nums < 100; second_nums++)
-		{
-			putchar (first_nums / 10 + '0');
-			putchar (first_nums % 10 + '0');
-			putchar (' ');
-			putchar (second_nums / 10 + '0');
-			putchar (second_nums % 10 + '0');
-			if (first_nums == 98 && second_nums ==99)
-			{
-				break;
-			}
-			putchar(',');
-			putchar (' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+  char alphabet;
+  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+    {
+      putchar(alphabet);
+    }
+  putchar('\n');
+  return (0);
 }
-
