b91b879ddd7d7cbbc81a293ed35996f02458f75c
Peegirl <timelessprecious2@gmail.com>
2022-07-03 01:32:05 -0700

    3-print

--- 0x01-variables_if_else_while/3-print_alphabetic.c
+++ 0x01-variables_if_else_while/3-print_alphabetic.c
@@ -1,43 +1,64 @@
-#include <stdio.h>
-
+nclude <stdio.h>
 #include <stdlib.h>
-
 #include <time.h>
 
-
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
-
 {
+	char low;
 
-		char low;
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
 
-
-
-			for (low = 'a'; low <= 'z'; low++)
-
-						putchar(low);
-
-				for (low = 'A'; low <= 'Z'; low++)
-
-							putchar(low);
-
-					putchar('\n');
+	return (0);
+}
 
 
 
-						return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
