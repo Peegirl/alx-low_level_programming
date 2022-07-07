a30af91f9af21d68681c47df3c1b0150c6ad1139
Peegirl <timelessprecious2@gmail.com>
2022-07-07 13:37:43 -0700

    main

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+fndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -9,4 +9,19 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
